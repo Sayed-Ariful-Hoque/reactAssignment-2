c45907c53b56230ce5c60ce0140743415bd91a46
Sayed-Ariful-Hoque <sayedarifulhoque171016xyz@gmail.com>
2025-05-18 11:54:15 +0600

    react assignment 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import TeamMemberCard from './commponent/TeamMemberCard'
 
 const App = () => {
-  const teamMember1 = [
+  const frontEndTeamMember = [
     { id: 3, name: "Ariful", email: "ariful@gmail.com", address: "mirpur 13" },
     { id: 2, name: "mehedi Vai", email: "mahedivai@gmail.com", address: "shewrapara" },
     { id: 1, name: "Daisy Apu", email: "daisyApu@gmail.com", address: "24 north kafrul" },
   ]
   return (
     <>
-      {teamMember1.map((member) => {
+      {frontEndTeamMember.map((member) => {
         return (
           <div>
             <TeamMemberCard key={member.id} id={member.id} name={member.name} email={member.email} address={member.address} />
--- src/commponent/TeamMemberCard.jsx
+++ src/commponent/TeamMemberCard.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const TeamMemberCard = ({ id, name, email, address }) => {
   return (
     <>
-      <h1>Name : {name}</h1>
-      <p>ID : {id}</p>
-      <p>Email: {email}</p>
-      <p>Address: {address}</p>
+      <h1><strong>Name : </strong>{name}</h1>
+      <p><strong>ID : </strong>{id}</p>
+      <p><strong>Email: </strong>{email}</p>
+      <p><strong>Address: </strong>{address}</p>
     </>
   )
 }
